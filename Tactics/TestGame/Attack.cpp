9c218a9b25b3d84d24f523e4da60bc24aaf9c7b6
Maurits Fassaert <maurits.fassaert@fei.com>
2019-02-16 11:01:13 +0100

    Empty unit test for attack, requires a lot of setup (world, actors)

---
+++ Tactics/TestGame/Attack.cpp
@@ -0,0 +1,23 @@
+#include <boost/test/unit_test.hpp>
+#include "Game/Attack.h"
+#include "Data.h"
+
+namespace Game
+{
+namespace Test
+{
+BOOST_AUTO_TEST_SUITE(AttackTest);
+
+
+
+BOOST_AUTO_TEST_CASE(Succeed)
+{
+
+}
+
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+}
+}  // Game::Test
