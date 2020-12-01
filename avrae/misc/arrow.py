324df18224cfdf1d1340338d03ce5d3a8e492a36
CountPauper <countpauper@gmail.com>
2020-12-01 09:00:58 +0100

    [avrae] generalized arrow snippet to be easier to adjust to other ammo

--- avrae/misc/arrow.py
+++ avrae/misc/arrow.py
@@ -1,30 +1,35 @@
 <drac2>
-# Find the quivers in the bags character variable
+# configure for ammunition type, capitalized
+ammo_name='Needle'
+bag_name='Case'
+# Find the ammo bags in the bags character variable
 if not exists('bags'):
-	err("Arrow counting only works after setting up a Quiver with `!bag $ Quiver`.")
+	err(f'{ammo_name} counting only works after setting up a {bag_name} with `!bag $ {bag_name}`')
+ammo_names=[ammo_name, ammo_name+'s']
+ammo_names+=[a.lower() for a in ammo_names]
+bags = load_json(bags)
+ammo_bags = [(b[1],sum([b[1].get(n,0) for n in ammo_names])) for b in bags if b[0].lower() == bag_name.lower()]
+if not ammo_bags:
+	err(f'You do not own a {bag_name}. Where are you getting that {ammo_name.lower()} from?')
 
-arrow_names=['Arrows','Arrow','arrows','arrow']
-bag = load_json(bags)
-quivers = [(b[1],sum([b[1].get(n,0) for n in arrow_names])) for b in bag if b[0].lower() == "quiver"]
-if not quivers:
-	err("You do not own a quiver. Where are you pulling that arrow from?")
-
-quivers = [(b,a) for (b,a) in quivers if a>0]
-total_remaining = sum([q for (_,q) in quivers])
+# count the ammo in the ammo bags
+ammo_bags = [(b,a) for (b,a) in ammo_bags if a>0]
+total_remaining = sum([q for (_,q) in ammo_bags])
 if total_remaining<=0:
-	err("There are no arrows left in any of your quivers.")
+	err(f'There are no {ammo_name.lower()}s left in any of your {bag_name.lower()}s.')
 
-# remove the arrow or arrows from the first quiver
-quiver = quivers[0][0]
-for arrow_name in arrow_names:
-	if remaining:=quiver.get(arrow_name,0):
+# remove the ammo from the first ammo bag
+ammo_bag = ammo_bags[0][0]
+for ammo in ammo_names:
+	if remaining:=ammo_bag.get(ammo,0):
 		remaining-=1
 		total_remaining-=1
 		if remaining:
-			quiver[arrow_name]=remaining
+			ammo_bag[ammo]=remaining
 		else:
-			quiver.pop(arrow_name)
+			ammo_bag.pop(ammo)
 		break
-set_cvar("bags", dump_json(bag))
+# apply the change to the chosen ammo_bag
+set_cvar("bags", dump_json(bags))
+return f'-f "{ammo_name}s Remaining"|{total_remaining}'
 </drac2>
--f "Arrows Remaining"|{{total_remaining}}
