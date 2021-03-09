301084e7dd661ab2c043417c82bec3420ca1abff
CountPauper <countpauper@gmail.com>
2021-03-09 13:35:52 +0100

    [avrae] npc edit with group, palette and defaults

--- avrae/ic/edit.py
+++ avrae/ic/edit.py
@@ -1,4 +1,5 @@
-tembed <drac2>
+embed <drac2>
+# TODO: update help -group, default name, color and palette
 UVAR_LOCAL = "npc_local_roster"
 local_roster = load_json(npc_local_roster) if uvar_exists(UVAR_LOCAL) else {}
 
@@ -12,11 +13,13 @@ RESERVED_WORDS = [
     "setchannel",
     "reset",
     "dump",
-    "sayimport"
+    "sayimport",
+    "group",
+    "share"
 ]
 
 out = []
-args = & ARGS &
+args = &ARGS&
 if len(args) == 0:
     return '-title "No arguments provided; see `!help npc edit`"'
 npc_id = args[0]
@@ -26,33 +29,39 @@ if npc_id in RESERVED_WORDS:
 else:
     parsed = argparse(args[1:])
     npc = local_roster[npc_id] if npc_id in local_roster else {
-        "name": "Give me a name!", "image": "", "color": ""}
+        "name": npc_id.title(), "image": "", "color": get('color')}
 
-    new_name = parsed.last("name")
-    new_image = parsed.last("image")
-    new_color = parsed.last("color")
-
-    if new_name is not None:
+    if new_name := parsed.last("name"):
         npc["name"] = new_name
-    if new_image is not None:
+    if new_image := parsed.last("image"):
         npc["image"] = new_image
-    if new_color is not None:
+    if new_color := parsed.last("color"):
+        palette = load_json(get_gvar('74f3bd10-7163-4838-ad27-344ad0fb6b83'))
+        new_color = palette.get(new_color.lower(), new_color)
         npc["color"] = new_color
+    if new_group := parsed.last("group"):
+        if new_group.lower() in ["reset","-"]:
+            if "group" in npc:
+                npc.pop("group")
+        else:
+            npc["group"] = new_group
 
     local_roster[npc_id] = npc
     set_uvar(UVAR_LOCAL, dump_json(local_roster))
 
     out.append(f'-title "Edited an NPC!"')
     out.append(f'-f "ID|{npc_id}"')
-    if (npc["name"]):
-        out.append(f'-f "Name|{npc["name"]}"')
-    if (npc["image"]):
-        out.append(f'-thumb {npc["image"]}')
-        out.append(f'-f "Image|{npc["image"]}"')
-        thumb = npc["image"]
-    if (npc["color"]):
-        npc_color = npc["color"]
-        out.append(f'-f "Color|{npc["color"]}"')
+    if npc_name:=npc["name"]:
+        out.append(f'-f "Name|{npc_name}"')
+    if npc_image:=npc["image"]:
+        out.append(f'-thumb {npc_image}')
+        out.append(f'-f "Image|[link]({npc_image})"')
+    if npc_color:=npc["color"]:
+        out.append(f'-f "Color|{npc_color}" -color {npc_color}')
+    if npc_group:=npc.get("group"):
+        out.append(f'-f "Group|{npc_group}"')
+    else:
+        out.append(f'-f Group|None')
 
 return " ".join(out)
 </drac2>
