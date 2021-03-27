1757f455dbe8495cb9f5fdf78e03b9505d2dad2d
CountPauper <countpauper@gmail.com>
2021-03-27 08:14:28 +0100

    [avrae] Ship, captain, crew

--- avrae/play/blackjack.py
+++ avrae/play/blackjack.py
@@ -1,9 +1,10 @@
 <drac2>
+# TODO: double aces at start (or after split) should also reduce
 cvar='Play'
 game_name='blackjack'
 state=load_json(get(cvar,'{}'))
-game=state.get(game_name,{'hand':[]})
-args=&ARGS&
+game=state.get(game_name,{})
+args="&*&"
 target=21
 cards=['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
 values={'a':1, 'A':11 , 'J':10, 'Q':10, 'K':10}
@@ -14,37 +15,48 @@ syntax=f'{ctx.prefix}{ctx.alias} {game_name} [hit|split|stand]'
 # start: get 2 cards
 if not args:
 	game = {'hand':[cards[randint(len(cards))],cards[randint(len(cards))]]}
+	hand=game.hand
 # hit: an extra card
 else:
-	hand=game.hand
+	hand=game.get('hand')
 	if not hand:
 		return f'echo No hand. Use `{ctx.prefix}{ctx.alias} {game_name}` to start.'
-	if args[0]=='hit':
+	if args=='hit':
 		hand.append(cards[randint(len(cards))])
-		if sum(values[card] for card in hand)>target:	# reduce an ace 11->1
-			if 'A' in hand:
-				hand[hand.index('A')]='a'
-		if sum(values[card] for card in hand)>target:	# bust
-			game['done'] = game.get('done', []) + [hand]
-			game['hand'] = None
-	elif args[0]=='split':
+	elif args=='split':
 		if game.get('split'):
 			return f'echo You already split.'
 		if len(hand)!=2:
 			return f'echo You can not split {hand}. Only the first two cards can be split.'
+		if hand==['a','A']:
+			hand[0]='A'
 		if values[hand[0]]!=values[hand[1]]:
 			return f'echo You can not split {hand}. The cards need to have the same value.'
 		game['split']=[hand[1] ,cards[randint(len(cards))]]
 		game['hand']=[hand[0] ,cards[randint(len(cards))]]
-	elif args[0]=='stand':
+		hand=game.hand
+	elif args=='stand':
 		game['done']=game.get('done',[])+[hand]
 		if split:=game.get('split'):
 			game['hand']=split
+			hand=game.hand
 			game.pop('split')
 		else:
 			game['hand']=None
 	else:
 		return f'echo `{syntax}`'
+
+if hand:
+	while 'A' in hand and sum(values[card] for card in hand)>target:	# reduce an ace 11->1
+		hand[hand.index('A')]='a'
+
+	if sum(values[card] for card in hand)>target:	# bust
+		game['done'] = game.get('done', []) + [hand]
+		if split := game.get('split'):
+			game['hand'] = split
+			game.pop('split')
+		else:
+			game['hand'] = None
 
 # persist game state
 state[game_name]=game
---
+++ avrae/play/ship.py
@@ -0,0 +1,45 @@
+<drac2>
+cvar='Play'
+game_name='ship'
+state=load_json(get(cvar,'{}'))
+game=state.get(game_name,{})
+args="&*&"
+turns=3
+syntax=f'{ctx.prefix}{ctx.alias} {game_name}'
+
+turn=game.get('turn',0)
+bank=game.get('bank',[])
+if turn>=turns:
+	turn=0
+	bank=[]
+
+rolls=[roll('1d6') for _ in range(5-len(bank))]
+target=[6,5,4]
+for t in target:
+	if t not in bank:
+		if t in rolls:
+			rolls.remove(t)
+			bank.append(t)
+		else:
+			break
+
+# persist
+
+if done:= all(t in bank for t in target):
+	state[game_name]={}
+else:
+	turn+=1
+	state[game_name]={'turn':turn, 'bank':bank}
+character().set_cvar(cvar,dump_json(state))
+
+ship=f'**{", ".join(str(r) for r in bank)}**, ' if bank else ''
+cargo=", ".join(str(r) for r in rolls)
+# output
+if done:
+	desc = f':sailboat: {ship} {cargo} = {sum(rolls)}'
+else:
+	desc= f'**Round:** {turn}/{turns} **Dice:** {ship} {cargo}'
+	if turn==turns:
+		desc+=' = 0'
+return f'embed -title "Ship, captain and crew" -desc "{desc}" -thumb https://upload.wikimedia.org/wikipedia/commons/9/99/Dice_-_1-2-4-5-6.jpg'
+</drac2>
