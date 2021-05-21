a13e1303029f5944778d8d050b58f849336605da
CountPauper <countpauper@gmail.com>
2021-05-21 15:35:25 +0200

    [avrae] DMI clarifications

--- avrae/dmi/dmi.py
+++ avrae/dmi/dmi.py
@@ -4,4 +4,7 @@ embed {{ cc="DM Inspiration"}}
 {{ f'-title "{name} receives DM inspiration!" '}}
 {{ f'-f "{cc}"|"{cc_str(cc)}" '}}
 {{ arg="&*&" }}
-{{ f'-desc "{arg}" ' if arg else '' }}
+<drac2>
+explain=f"""Use the `dmi` snippet in an `{ctx.prefix}attack`, `{ctx.prefix}check` or `{ctx.prefix}save` to your advantage, for instance `{ctx.prefix}save death dmi`. You can also reward it to another player using `{ctx.prefix}{ctx.alias} reward <target>`"""
+return f'-desc "*{arg}*\n{explain}" ' if arg else f'-desc "{explain}"'
+</drac2>
--- avrae/dmi/reward.py
+++ avrae/dmi/reward.py
@@ -6,7 +6,7 @@ if not char.cc_exists(cc) or char.get_cc(cc)<=0:
 	return f'echo You need to have Dungeon Master Inspiration to reward another player. Use `!{alias_name}` if you\'re supposed to have it.'
 target="&*&"
 if not target:
-	return f'echo Use `!{ctx.alias} <name>` to use your own Dungeon Master Inspiration to reward another player.'
+	return f'echo Use `!{ctx.alias} reward <name>` to use your own Dungeon Master Inspiration to reward another player.'
 char.mod_cc(cc,-1)
 return f'embed -title "{name} rewards {target} with Dungeon Master Inspiration" -desc "{target} can use `!{alias_name}` to receive it and the `dmi` snippet to use it." -f "{cc}|{char.cc_str(cc)}"'
 </drac2>
