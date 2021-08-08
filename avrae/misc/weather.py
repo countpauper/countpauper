4e6cceb7df4ded807c287ac867b96297ce24b44c
CountPauper <countpauper@gmail.com>
2021-08-08 08:15:16 +0200

    [avrae] !weather help and small fixed

--- avrae/misc/weather.py
+++ avrae/misc/weather.py
@@ -1,36 +1,30 @@
 !alias weather <drac2>
-args=&ARGS&
-cv='climate'
-x=args[0] if args else get(cv,'list')
-n="\n"
+args=argparse([a.lower() for a in &ARGS&])
+cv,n='climate',"\n"
 g=load_json(get_gvar('16b912b6-817a-4923-9f66-f765bd69f346'))
 C=g.climate
-if x in 'list':
+if args.get('help'):
+	return f'echo `{ctx.prefix}{ctx.alias} [<climate>] [<season>] [-h <humidity bonus>] [-w <wind speed bonus>] [-t <temperature bonus>]`'
+if args.get('list'):
 	return f'embed -title "Supported Climates" -desc "{n.join(c.title() for c in C.keys())}" -footer "{ctx.prefix}{ctx.alias} [climate] [season]"'
-x2=([k for k in C.keys() if x.lower() in k]+[None])[0]
+x2=([k for k in C.keys() if args.last(k)]+[get(cv)])[0]
 if not x2:
-	return f'echo Unknown climate `{x}`'
+	return f'echo No default climate set'
 set_uvar(cv,x2)
 
 sns=g.season
-s=None
-if len(args)>1:
-	sa=args[1].lower()
-	p=([k for k in sns.keys() if sa in k]+[None])[0]
-	if not p:
-		return f'echo Unknown season `{sa}`'
-	s=sns[p].idx
-else:
-	dv = f'Date_{ctx.guild.id}' if ctx.guild else 'Date'
-	z=int(get(dv,randint(366)))%365
-	ss = [(n,sn.idx) for n,sn in sns.items() if (sn.start<sn.end and z>=sn.start and z<=sn.end) or (sn.end<sn.start and (z<=sn.end or z>=sn.start))]
-	p,s=ss[0]
+dv = f'Date_{ctx.guild.id}' if ctx.guild else 'Date'
+z = int(get(dv, randint(366))) % 365
+ss = [(n, sn.idx) for n, sn in sns.items() if
+	  (sn.start < sn.end and z >= sn.start and z <= sn.end) or (sn.end < sn.start and (z <= sn.end or z >= sn.start))]
+p,s=ss[0]
+p=([k for k in sns.keys() if args.get(k)]+[p])[0]
+s=sns[p].idx
 
-args=argparse(args)
 c=C[x2]
 th=roll('+'.join([c.th[s]]+args.get('t')))
 tl=roll('+'.join([c.tl[s]]+args.get('t')))
-r=roll('+'.join([c.humid[s]]+args.get('h')))
+r=max(1,roll('+'.join([c.humid[s]]+args.get('h'))))
 w=max(0,roll('+'.join([c.wind[s]]+args.get('w'))))
 wd=roll("1d8")
 dir=g.wind
@@ -38,7 +32,7 @@ c=g.weather
 
 s=p.title()
 bft=[0,1,4,8,13,19,25,32,39,47,55,64,73]
-bft=min(idx for idx,mpb in enumerate(bft) if mpb>=w)
+bft=min(idx for idx,mpb in enumerate(bft) if mpb>=w) if w<bft[-1] else 12
 t=f'-title "Today\'s Weather:  {x2.title()}, {s}"'
 if tl>0:
 	weather=[w for v,w in g.weather.items() if r>int(v)][-1]
