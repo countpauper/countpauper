d2e61f32042a51b82ea9cc7a1e205171b7d72b72
CountPauper <countpauper@gmail.com>
2021-11-23 18:29:38 +0100

    [avrae] combat state configurable fields and added some options

--- avrae/state/combat.py
+++ avrae/state/combat.py
@@ -11,7 +11,6 @@ C=combat()
 if not C:
 	return f'techo 8 The current channel is not in combat.'
 
-
 result=[]
 roundstr=[':zero:',':one:',':two:',':three:',':four:',':five:',':six:',':seven:',':eight:',':nine:',':keycap_ten:']
 header=f'Combat Round {C.round_num if C.round_num>=len(roundstr) else roundstr[C.round_num]} in "{ctx.channel.name}"'
@@ -38,6 +37,20 @@ else:
 			timeout=None
 	else:
 		return f'echo Timeout argument `-t {timeout}` syntax error. The value must be a number.'
+
+cvar_name=f'{ctx.alias}_combat'
+fields=[]
+store_cvar=False
+for a in &ARGS&:
+	if a.startswith('-'):
+		break
+	else:
+		fields.append(a.lower())
+		store_cvar = True
+fields = fields or load_json(get(cvar_name,'{}')).get('fields',['team','heart','melee','ranged','cantrip','spell','condition','effect'])
+
+if store_cvar:
+	character().set_cvar(cvar_name,dump_json(dict(fields=fields)))
 
 alphabet='abcdefghijklmnopqrstuvwxyz'
 x_table=[' ']+[c for c in alphabet]+[f'a{c}' for c in alphabet]+[f'b{c}' for c in alphabet]
@@ -76,39 +89,61 @@ conditions=config.conditions
 
 dbg=''
 heartstr = [':skull:', ':broken_heart:', ':hearts:', ':heartbeat:', ':heartpulse:']
-distancestr=['', ':dart:', ':bow_and_arrow:']
 for c in C.combatants:
 	if c.name in dm_combatants:
 		continue
-	active=C.current and c.name==C.current.name
-	props=[]
-	if c.max_hp:
-		health=(c.hp>0) + (c.hp>=c.max_hp/7) + (c.hp>=c.max_hp/2) + (c.hp>=c.max_hp)
-		props.append(f'{heartstr[health]}{":heavy_plus_sign:" if c.temp_hp>0 else ""}')
-
-	for e in c.effects:
-		if prop:=[c_str for cond,c_str in conditions.items() if cond in e.name.lower()]:
-			props.append(''.join(prop))
-		elif e.conc and e.children:
-			props.append(':brain:')
-		else:
-			props.append(e.name)
-
-	desc='npc' if c.levels.get('Monster')>0.0 else c.levels	# TODO: option
-
-	you,distance='',''
-	if C.me and C.me.name==c.name:
-		you='(You)'
+	# precompute some generic stuff:
+	if C.me and (me_loc := location.get(C.me.name)) and (c_loc := location.get(c.name)):
+		dx = me_loc.x - c_loc.x
+		dy = me_loc.y - c_loc.y
+		distance = sqrt(dx * dx + dy * dy) * 5.0
 	else:
-		you='(Yours)' if c.controller == ctx.author.id else f'({c.group})' if c.group else 'DM' if c.controller==dm_id else ''
-		if C.me and (me_loc:=location.get(C.me.name)) and (c_loc:=location.get(c.name)):
-			dx=me_loc.x-c_loc.x
-			dy=me_loc.y-c_loc.y
-			d=sqrt(dx*dx + dy*dy)*5.0
-			distance = ':crossed_swords:' if d<=ranges.reach else distancestr[(d<=ranges.range) + (d<=ranges.max_range)]
-			distance += ':magic_wand:' if d<ranges.get('cantrip',-1) else ''
-			distance += ':sparkles:' if d<ranges.get('spell',-1) else ''
-	result.append(f'{":arrow_forward:" if active else ":white_large_square:"} **{c.name}** {you} {distance}{" - " if distance and props else ""}{", ".join(props)}')
+		distance = None
+	active=C.current and c.name==C.current.name
+
+	#desc='npc' if c.levels.get('Monster')>0.0 else c.levels	# TODO: option
+	props=[]
+	for field in fields:
+		if field=='heart' and c.max_hp:
+			health=(c.hp>0) + (c.hp>=c.max_hp/7) + (c.hp>=c.max_hp/2) + (c.hp>=c.max_hp)
+			props.append(f'{heartstr[health]}{":heavy_plus_sign:" if c.temp_hp>0 else ""}')
+		elif field=='hp':
+			props.append(c.hp_str())
+		elif field.startswith('effect'):
+			for e in c.effects:
+				if prop:=[c_str for cond,c_str in conditions.items() if cond in e.name.lower()]:
+					props.append(''.join(prop))
+				elif e.conc and e.children:
+					props.append(':brain:')
+				else:
+					props.append(e.name)
+		elif distance is not None:
+			if field == 'distance':
+				props.append(f'{int(distance)}ft')
+			elif field=='melee':
+				if distance <= ranges.reach:
+					props.append(':crossed_swords:')
+			elif field=='ranged':
+				if distance<= ranges.range:
+					props.append(':dart:')
+				elif distance <= ranges.max_range:
+					props.append(':bow_and_arrow:')
+			elif field=='cantrip':
+				if distance <= ranges.get('cantrip', -1):
+					props.append(':magic_wand:')
+			elif field=='spell':
+				if distance <= ranges.get('spell', -1):
+					props.append(':sparkles:')
+		elif field=='team':
+			if C.me and C.me.name==c.name:
+				props.append('(You)')
+			elif c.controller == ctx.author.id:
+				props.append('(Yours)')
+			elif c.controller==dm_id:
+				props.append('(DM)')
+			elif c.group:
+				props.append(f'({c.group})')
+	result.append(f'{":arrow_forward:" if active else ":white_large_square:"} **{c.name}** {" - " if props else ""}{" ".join(props)}')
 
 # limit result size to 4096 discord embed description limit, discord also cuts the field short with too many smileys
 # and the whole embed can only be 6000
