bc04dd270a7e04f7bc81d2a7166bbe8a159a3dba
CountPauper <countpauper@gmail.com>
2023-01-21 16:49:14 +0100

    [math] solve linear differential equation and diagonalize matrix

--- Math/Solvers/poly.py
+++ Math/Solvers/poly.py
@@ -36,12 +36,26 @@ print(solve(1, 0, -1))
 print(solve(2, 4, -4))
 
 # https://www.youtube.com/watch?v=fi54Hz5TiWI&list=PLMrJAkhIeNNTYaOnVI3QpH7jgULnAmvPA&index=10
+def solve_lde(A):
+	eVals, eVecs = np.linalg.eig(A)
+	return eVals, eVecs
+
 def solve_lde_with_eigen(*terms):
 	a,b,c= terms  # ax" + bx' +cx = 0
 	# x" = -(b/a)x' - (c/a)x
-	A= np.array([[0, 1], [-c/a, -b/a]]) # [v',v"] = A * [v, v']
-	eigen = np.linalg.eig(A)
-	return eigen[0]
+	# A=  # [v',v"] = A * [v, v']
+	return solve_lde(np.array([[0, 1], [-c/a, -b/a]]))
 
 print(f"eigen: { solve_lde_with_eigen(1,-3,2)}")
 print(f"eigen: { solve_lde_with_eigen(1, 1,-2)}")
+
+
+def diagonalize(A):
+	eVals, eVecs = np.linalg.eig(A)
+	return np.diag(eVals), eVecs
+A=np.array([[8, 10, 10], [0, 3, 0], [-5, -10, -7]])
+D, S = diagonalize(A)
+Sinv=np.linalg.inv(S)
+
+print(f'Diagonal({A})) = { diagonalize(A)}')
+print(S.dot(D.dot(Sinv)))
