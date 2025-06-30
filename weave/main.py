202f99a88d6390003cb0581f87850461e59d3c70
countpauper <countpauper@gmail.com>
2025-06-30 06:39:18 +0200

    [Weave] Make exe

--- weave/main.py
+++ weave/main.py
@@ -4,20 +4,25 @@ import argparse
 def resize_font(height):
     return pygame.font.SysFont('Arial Rounded MT Bold', int(height*0.8)), int(height*0.1)
 
+# parse the arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=argparse.FileType('r'), default="lines.txt", nargs='?')
+parser.add_argument("-nr", action='store_true', required=False)
 args = parser.parse_args()
 
+# Load the lines
 lines = args.file.readlines()
 args.file.close()
 lines = [line.strip() for line in lines]
 
+# initialize the window
 pygame.init()
 screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 height = screen.get_size()[1]
 GAME_FONT, offset = resize_font(screen.get_size()[1])
 running =  True
 
+# Main loop
 line = 0
 while running:
     for event in pygame.event.get():
@@ -31,7 +36,11 @@ while running:
             GAME_FONT, offset = resize_font(event.h)
         
     screen.fill((255,255,255))
-    text_surface = GAME_FONT.render(lines[line], True, (0, 0, 0))
+    if args.nr:
+        text = f"{line+1}:{lines[line]}"
+    else:
+        text = lines[line]
+    text_surface = GAME_FONT.render(text, True, (0, 0, 0))
     screen.blit(text_surface, (40, offset))
 
     pygame.display.flip()
---
+++ weave/make_installer.sh
@@ -0,0 +1,5 @@
+pip install pyinstaller
+# pyinstaller main.py
+pyinstaller -F main.py -n treadle_counter.exe 
+#pyinstaller -F --paths=.venv/Lib/site-packages  main.py
+#pip install pynput==1.6.8
